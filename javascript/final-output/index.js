c477ae262423f49c674bc8f3d0ac9655792b8020
sjb1001 <bronosasean.12@gmail.com>
2024-12-16 14:07:03 +0800

    Update

--- javascript/final-output/index.js
+++ javascript/final-output/index.js
@@ -3,34 +3,73 @@ const url = "http://localhost:8080";
 const content = document.getElementById("content");
 const container = document.querySelector(".container");
 
-async function getChartData() {
-  const response = await fetch(`${url}/chart`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      backgroundColor: "#fff",
-      width: 500,
-      height: 300,
-      devicePixelRatio: 1.0,
-      chart: {
-        type: "bar",
-        data: {
-          labels: [2012, 2013, 2014, 2015, 2016],
-          datasets: [
-            {
-              label: "Users",
-              data: [120, 60, 50, 180, 120],
-            },
-          ],
-        },
-      },
-    }),
-  });
+function clearContent() {
+  container.innerHTML = "";
+}
 
-  const data = await response.json();
-  console.log(data);
+function changeView(view) {
+  clearContent();
+
+  if (view === "graph") {
+  } else if (view === "color") {
+    container.innerHTML = `
+      <div class="content-item">
+        <h4 class="title">Select a color</h4>
+        <input type="color">
+      </div>
+      <div class="line"></div>
+    `;
+    const input = container.querySelector("input");
+    input.addEventListener("input", function (e) {
+      console.log(e.target.value);
+    });
+  } else if (view === "compile") {
+    container.innerHTML = `
+    <form id="form-code">
+      <select name="language">
+        <option value="">Select a language</option>
+        <option value="java">Java</option>
+        <option value="cpp">C++</option>
+        <option value="py">Python</option>
+        <option value="js">NodeJS</option>
+      </select>
+      <textarea name="code"></textarea>
+      <div class="line"></div>
+      <button type="submit" class="secondary-btn">Run</button>
+    </form>
+    `;
+
+    const form = document.getElementById("form-code");
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const formData = new FormData(form);
+
+      const data = {
+        code: formData.get("code"),
+        language: formData.get("language"),
+      };
+
+      const response = compileCode(data);
+      alert(response);
+    });
+  }
+}
+
+async function getChartData() {
+  const response = await fetch(
+    `https://quickchart.io/chart?c={type:'bar',data:{labels:[2012,2013,2014,2015, 2016],datasets:[{label:'Users',data:[120,60,50,180,120]}]}}
+`,
+    {
+      method: "GET",
+    }
+  );
+
+  const data = await response.arrayBuffer();
+
+  const img = document.createElement("img");
+  img.src = URL.createObjectURL(new Blob([data], { type: "image/png" }));
+  container.appendChild(img);
 }
 
 async function generateGraph() {
@@ -52,3 +91,19 @@ async function generateGraph() {
   await getChartData();
 }
 
+async function compileCode(payload) {
+  try {
+    const response = await fetch(`${url}/compile`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: payload,
+    });
+
+    const result = await response.json();
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  }
+}
--- javascript/final-output/server.js
+++ javascript/final-output/server.js
@@ -1,50 +1,75 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+var qs = require("qs");
+
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const fakerUrl = "https://fakerapi.it/api/v2/companies?_quantity=5";
-const chartUrl = "https://quickchart.io/chart"
+const chartUrl = "https://quickchart.io/chart";
+const codexUrl = "https://api.codex.jaagrav.in";
 
-app.get('/', (req, res) => {
-  return res.status(200).send('Hey! What are you doing here?!');
+app.get("/", (req, res) => {
+  return res.status(200).send("Hey! What are you doing here?!");
 });
 
-app.get('/generate', async (req, res) => {
+app.get("/generate", async (req, res) => {
   try {
     const response = await axios.get(fakerUrl, {
       headers: {
         "Content-Type": "application/json",
-      }
+      },
     });
 
     return res.status(200).json(response.data);
   } catch (e) {
-    return res.status(500).json({message: e.message || "An error occurred"});
+    return res.status(500).json({ message: e.message || "An error occurred" });
   }
 });
 
-app.post('/chart', async (req, res) => {
+app.post("/chart", async (req, res) => {
   try {
     const response = await axios.post(chartUrl, {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(req.body),
+      data: JSON.stringify(req.body),
     });
 
-    console.log(response.data)
     return res.status(200).json(response.data);
   } catch (e) {
-    return res.status(500).json({message: e.message || "An error occurred"});
+    return res.status(500).json({ message: e.message || "An error occurred" });
   }
-})
+});
+
+app.post("/compile", async (req, res) => {
+  try {
+    const { code, lang } = req.body;
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("code", code);
+    encodedParams.append("language", lang);
+
+    const response = await axios.post(codexUrl, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: JSON.stringify(req.body),
+    });
+
+    return res.status(200).json(JSON.stringify(response.data));
+  } catch (e) {
+    return res.status(500).json({ message: e.message || "An error occurred" });
+  }
+});
 
 app.listen(8080, () => {
-  console.log('Server started on http://localhost:8080');
+  console.log("Server started on http://localhost:8080");
 });
